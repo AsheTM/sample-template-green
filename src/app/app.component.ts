493fcdd05bf041c08babb88b64f6a08160a7a172
Haithem <haithemalidaoud@gmail.com>
2018-12-16 17:26:51 +0000

    make it better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SlidesService } from './services/slides.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'prezDOHA';
+
+  constructor(private slides: SlidesService){}
 }
--- src/app/services/slides.service.ts
+++ src/app/services/slides.service.ts
@@ -5,5 +5,25 @@ import { Injectable } from '@angular/core';
 })
 export class SlidesService {
 
+  public slides: any = {
+    one: 0,
+    two: 1,
+    three: 1,
+    four: 1,
+    five: 1,
+    six: 1
+  }
+
   constructor() { }
+
+  public switchTo(slideNumber: any){
+    let slide: any = slideNumber;
+    if(typeof slide == "number")
+      slide = Object.entries(this.slides)[slide - 1][0];
+    let curr = false;
+    for(let k in this.slides){
+      if(slide == k)curr = true;
+      this.slides[k] = !curr && -1 || slide == k && 0 || slide != k && curr && 1;
+    }
+  }
 }
