dee0c44ffaefc81786e378e82319643ea590e70b
Haithem <haithemalidaoud@gmail.com>
2018-12-16 12:35:04 +0000

    Theme Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlideOneComponent } from './slide-one/slide-one.component';
+import { SlideTwoComponent } from './slide-two/slide-two.component';
+import { SlideThreeComponent } from './slide-three/slide-three.component';
+import { HeaderComponent } from './header/header.component';
+import { SlideFourComponent } from './slide-four/slide-four.component';
+import { SlideFiveComponent } from './slide-five/slide-five.component';
+import { SlideSixComponent } from './slide-six/slide-six.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideOneComponent,
+    SlideTwoComponent,
+    SlideThreeComponent,
+    HeaderComponent,
+    SlideFourComponent,
+    SlideFiveComponent,
+    SlideSixComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/slide-six/slide-six.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-six',
+  templateUrl: './slide-six.component.html',
+  styleUrls: ['./slide-six.component.scss']
+})
+export class SlideSixComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
