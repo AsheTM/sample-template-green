19bb226707128b9742a705697bf2af0dfd4a3028
haithemalidaoud@gmail.com <haithemalidaoud@gmail.com>
2018-12-19 23:20:41 +0000

    make it better

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/prezDOHA'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req, res) => {res.sendFile(path.join(__dirname + '/dist/prezDOHA/index.html'));});
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, (req, res) => console.info("Listeniing!"));
